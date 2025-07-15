6148fbaebade4fc985f2d31b7895f7d8466b79d9
Deni Thakulla <horizon9868@gmail.com>
2025-07-15 11:45:49 +0545

    Added cart button on header and routes where required

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,18 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { PrimaryButton } from "../primary-button/primary-button";
+import { CartService } from '../../services/cart';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [PrimaryButton],
+  imports: [PrimaryButton,RouterLink],
   template: `
     <div class="bg-slate-100 px-4 py-3 shadow-md font-semibold flex justify-between items-center">
-      <span class="text-xl">My Store</span>
-      <app-primary-button label="Cart" (btnClicked)="showButtonClicked()"/>
+      <span class="text-xl cursor-pointer" routerLink="/">My Store</span>
+      <app-primary-button 
+      [label]="'Cart (' + cartService.cart().length + ')'" 
+      (btnClicked)="showButtonClicked()"
+      routerLink="/cart"/>
     </div>
   `,
   styles: ``
 })
 export class Header {
+    cartService = inject(CartService);
     showButtonClicked() {
         console.log('Cart button clicked!');
     }
